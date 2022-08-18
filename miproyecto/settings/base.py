14d39c1274e5527e92c0a14e8d804f555267e620
Lucas <lucasexrojas@gmail.com>
2022-08-18 13:58:35 -0300

    changes in postgres

--- miproyecto/settings/base.py
+++ miproyecto/settings/base.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'miproyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bd_proyecto',
+        'USER': 'postgres',
+        'PASSWORD': 'Namecuseis1',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
