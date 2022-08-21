be60e6edd38a6167fcad2a7c49b96ecb74518fd2
natanatalina <shotaoouji@gmail.com>
2022-08-21 14:07:48 -0300

    default

--- miproyecto/settings/base.py
+++ miproyecto/settings/base.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
-    'blog', #agregado blog
+    'blog',
+    'usuarios',
 
 ]
 
@@ -78,22 +79,14 @@ WSGI_APPLICATION = 'miproyecto.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-<<<<<<< HEAD
-   'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bd_proyecto',
-        'USER': 'postgres',
-        'PASSWORD': 'Nameacuseis1',
-=======
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'bd_proyecto',
         'USER': 'postgres',
         'PASSWORD': 'Namecuseis1',
->>>>>>> refs/remotes/origin/master
         'HOST': '127.0.0.1',
         'PORT': '5432',
-    }
+    },
 }
 
 # Password validation
@@ -144,3 +137,6 @@ MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'media'),
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
