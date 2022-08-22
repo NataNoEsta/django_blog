1024fbe812047c8f159bff88279000c0f19c00b4
natanatalina <shotaoouji@gmail.com>
2022-08-22 18:04:38 -0300

    default

--- usuarios/views.py
+++ usuarios/views.py
@@ -4,5 +4,5 @@ from django.urls import reverse_lazy
 
 class RegistroUsuario(generic.CreateView):
     form_class = UserCreationForm
-    template_name = 'register/signup.html'
+    template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
