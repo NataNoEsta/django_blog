c4f21921450dedc3ecda8463929fc1be879b1b5d
natanatalina <shotaoouji@gmail.com>
2022-08-28 18:51:04 -0300

    Agregada social_share

--- miproyecto/settings/base.py
+++ miproyecto/settings/base.py
@@ -9,13 +9,11 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
 import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -27,6 +25,7 @@ SECRET_KEY = 'django-insecure-)6!p8#6+_-igln(t**bf%fo-$dbe+v@)906dshswi0=14st9y2
 DEBUG = True
 
 ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['evening-castle-75145.herokuapp.com']
 
 
 # Application definition
@@ -41,6 +40,7 @@ INSTALLED_APPS = [
     'bootstrap4',
     'blog',
     'usuarios',
+    'django_social_share',
 
 ]
 
