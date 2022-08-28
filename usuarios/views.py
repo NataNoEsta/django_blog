6d0287795a56b013af23d9185f6fa71766780a30
natanatalina <shotaoouji@gmail.com>
2022-08-27 21:40:23 -0300

    Modificado user form

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,9 @@
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .forms import CrearUsuario
 
 class RegistroUsuario(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = CrearUsuario
     template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
