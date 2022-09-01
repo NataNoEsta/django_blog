6d7cc6be904c7d3dec06e36d594504431a646ebc
natanatalina <shotaoouji@gmail.com>
2022-09-01 14:03:54 -0300

    .

--- miproyecto/settings/base.py
+++ miproyecto/settings/base.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)6!p8#6+_-igln(t**bf%fo-$dbe+v@)906dshswi0=14st9y2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bloggrupo1comi5.herokuapp.com']
 
 # Application definition
 
