d37d24f37e43af0ae66da62581813c302f53f8ed
natanatalina <shotaoouji@gmail.com>
2022-09-01 13:40:14 -0300

    .

--- miproyecto/settings/base.py
+++ miproyecto/settings/base.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)6!p8#6+_-igln(t**bf%fo-$dbe+v@)906dshswi0=14st9y2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
