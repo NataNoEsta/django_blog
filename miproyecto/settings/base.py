17d2cdc8f93db2dd4f7c1ed7e5f2c670c229d46e
natanatalina <shotaoouji@gmail.com>
2022-09-01 11:08:20 -0300

    Heroku fix

--- miproyecto/settings/base.py
+++ miproyecto/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 import django_heroku
+import dj_database_url
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,13 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
     os.path.join(os.path.dirname(BASE_DIR), 'miproyecto\static'),
 ]
-     
-    
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'    
+ 
     
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'media'),
